a152495d5d1fe2516ffbc6c7ccb1410a96461c1b
Montserrat <A01668163@tec.mx>
2025-10-30 12:59:46 -0600

    Se modifico la velocidad de los fantasmas en todos los vectores y en onkey con variantes de 15, -15 y 10, -10 y en aim

--- pacman.py
+++ pacman.py
@@ -17,7 +17,7 @@ from freegames import floor, vector
 state = {'score': 0}
 path = Turtle(visible=False)
 writer = Turtle(visible=False)
-aim = vector(5, 0)
+aim = vector(15, 10)
 pacman = vector(-40, -80)
 ghosts = [
     [vector(-180, 160), vector(5, 0)],
